acbdbb0942cf32f09a2f97391413c56d92470720
David Zhou <z5157517@student.unsw.edu.au>
2018-09-28 13:07:20 +1000

    Removed profile functions from main.c

--- main.c
+++ main.c
@@ -27,37 +27,6 @@ void port_Init()
     PORTB &= ~(0b111);      // Sets the outputs as off
 }
 
-
-void Profile_RESET(void){
-     
-    // Declares a Profile with 0 for everything
-    Profile profile;
-    profile.alarm1  = 0x00;
-    profile.alarm2  = 0x00;
-    profile.alarm3  = 0x00;
-    profile.alarmStatus1 = 0;
-    profile.alarmStatus2 = 0;
-    profile.alarmStatus3 = 0;
-    profile.feed1 = 0;
-    profile.feed2 = 0;
-    profile.feed3 = 0;
-    
-    // Declare a Profiles with the zero Profile all three profiles
-    Profiles profiles;
-    profiles.profile1 = profile;
-    profiles.profile2 = profile;
-    profiles.profile3 = profile;
-
-    // Write the profile to EEPROM memory
-    char eeprom_address = 0x00;
-    EEPROM_WriteNBytes(eeprom_address, &profiles, sizeof(Profiles));
-} 
-
-void Profile_LOAD(Profiles* profiles){
-    char eeprom_address = 0x00;
-    EEPROM_ReadNBytes(eeprom_address, profiles, sizeof(Profiles));
-}
-
 int main(void)
 {
     port_Init();
